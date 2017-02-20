d08503707c091f9dbd5138690760206c3ca16a83
Micah Walter <micah@micahwalter.com>
2017-02-20 16:02:13 -0500

    just getting started

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Setting up kesnerwalter for the first time"
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install git -y
+
+git clone git@github.com:kesnerwalter/ops-ubuntu.git /vagrant/ops-ubuntu
+
+/vagrant/ops-ubuntu/vagrant/setup.sh
