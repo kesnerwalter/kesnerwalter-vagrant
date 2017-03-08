0aa40750fa5ff31e2e838360255f29b43c6de08d
Micah Walter <micah@micahwalter.com>
2017-03-08 10:49:48 -0500

    moved ops to an ops folder

--- setup.sh
+++ setup.sh
@@ -7,12 +7,20 @@ sudo apt-get upgrade -y
 
 sudo apt-get install git -y
 
-git clone git@github.com:kesnerwalter/ops-ubuntu.git /vagrant/ops-ubuntu
+if [ ! -d /vagrant/ops ]
+then
+    sudo mkdir /vagrant/ops
+    sudo chmod 700 /vagrant/ops
+    sudo chown root /vagrant/ops
+fi
 
-/vagrant/ops-ubuntu/vagrant/setup.sh
-/vagrant/ops-ubuntu/vagrant/setup-apache.sh
-/vagrant/ops-ubuntu/vagrant/setup-php.sh
-/vagrant/ops-ubuntu/vagrant/setup-mysql.sh
+
+git clone git@github.com:kesnerwalter/ops-ubuntu.git /vagrant/ops/ops-ubuntu
+
+/vagrant/ops/ops-ubuntu/vagrant/setup.sh
+/vagrant/ops/ops-ubuntu/vagrant/setup-apache.sh
+/vagrant/ops/ops-ubuntu/vagrant/setup-php.sh
+/vagrant/ops/ops-ubuntu/vagrant/setup-mysql.sh
 
 sudo service apache2 restart
 
