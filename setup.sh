530b8304fea6853f102c5009c3242f6a656e9114
Micah Walter <micah@micahwalter.com>
2017-03-08 11:00:42 -0500

    installing main site

--- setup.sh
+++ setup.sh
@@ -10,8 +10,6 @@ sudo apt-get install git -y
 if [ ! -d /vagrant/ops ]
 then
     sudo mkdir /vagrant/ops
-    sudo chmod 700 /vagrant/ops
-    sudo chown root /vagrant/ops
 fi
 
 
@@ -23,5 +21,14 @@ git clone git@github.com:kesnerwalter/ops-ubuntu.git /vagrant/ops/ops-ubuntu
 /vagrant/ops/ops-ubuntu/vagrant/setup-mysql.sh
 
 sudo service apache2 restart
+
+echo "Installing the main site
+"
+if [ ! -d /vagrant/code ]
+then
+    sudo mkdir /vagrant/code
+fi
+
+git clone git@github.com:kesnerwalter/kesnerwalter.git /vagrant/code/kesnerwalter
 
 exit 0
